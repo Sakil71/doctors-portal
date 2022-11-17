584265086c1637c8232a65c1c7dab49121ef9520
Shakil Ahamed <sakilahmed7810@gmail.com>
2022-11-18 00:53:10 +0600

    Added Sign Up

--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,37 +1,57 @@
-import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState('');
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleLogin = data => {
+        console.log(data);
+    }
 
     return (
         <section className='h-[400px] flex justify-center items-center my-16'>
             <div>
                 <h1 className='text-4xl text-center font-bold'>Login</h1>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                <form onSubmit={handleSubmit(handleLogin)}>
 
+                    {/* For Email */}
                     <div className="form-control w-96">
                         <label className="label"><span className="label-text">Email</span></label>
-                        
-                        <input {...register("email")} type="email" className="input input-bordered w-full" placeholder='email' />
+
+                        <input {...register("email", { required: "Email is required" })}
+                         type="email" 
+                         className="input input-bordered w-full" 
+                         placeholder='email' 
+                         />
+
+                        {errors.email && <p role="alert" className="text-error mt-4">{errors.email?.message}</p>}
                     </div>
 
+                    {/* For Password */}
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text">Password</span></label>
 
-                        <input {...register("password")} type="password" className="input input-bordered w-full" placeholder='password' />
+                        <input {...register("password", { required: "Password is required",
+                        minLength:{value:6, message:"Password at least 6 characters"} 
+                        })} 
+                        type="password" 
+                        className="input input-bordered w-full" 
+                        placeholder='password' 
+                        />
+
+                        {errors.password && <p role="alert" className="text-error mt-4">{errors.password?.message}</p>}
 
                         <label className="label"><span className="label-text">Forget Password?</span></label>
                     </div>
 
-                    <p>{data}</p>
                     <input className='btn btn-primary mt-5 w-full' type="submit" value={"Login"} />
                 </form>
+
                 <p>New to Doctors Portal? <Link className='text-secondary' to='/signup'>Create An Account</Link></p>
 
                 <div className="divider">OR</div>
+
+                {/* For Google Login */}
                 <button className='btn btn-outline w-full'>Continue With Google</button>
             </div>
         </section>
--- src/Pages/Shared/SignUp/SignUp.js
+++ src/Pages/Shared/SignUp/SignUp.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-    const [data, setData] = useState('');
-    const {register, handleSubmit} = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleLogin = data => {
+        console.log(data);
+    }
 
     return (
         <section className='h-[400px] flex justify-center items-center my-16'>
             <div>
                 <h1 className='text-4xl text-center font-bold'>Sign Up</h1>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                <form onSubmit={handleSubmit(handleLogin)}>
 
                     <div className="form-control w-96">
                         <label className="label"><span className="label-text">Email</span></label>
 
-                        <input {...register("email")} type="email" className="input input-bordered w-full" placeholder='email' />
+                        <input {...register("email", {
+                            required: 'Email is required',
+                        })}
+                            type="email"
+                            className="input input-bordered w-full"
+                            placeholder='email' />
+
+                        {errors.email && <span className="text-error mt-3">{errors.email?.message}</span>}
                     </div>
 
+                    {/* For Password */}
                     <div className="form-control w-full">
                         <label className="label"><span className="label-text">Password</span></label>
 
-                        <input {...register("password")} type="password" className="input input-bordered w-full" placeholder='password' />
+                        <input {...register("password", {
+                            required: "Password is required",
+                            minLength: {value: 6, message: "Password at least 6 characters"}
+                        })} type="password"
+                            className="input input-bordered w-full"
+                            placeholder='password' />
+
+                        {errors.password && <span className="text-error mt-3">{errors.password?.message}</span>}
 
                     </div>
 
-                    <p>{data}</p>
                     <input className='btn btn-primary mt-5 w-full' type="submit" value={"Sign Up"} />
                 </form>
+
                 <p>Already have an account? <Link className='text-secondary' to='/login'>Login</Link></p>
 
                 <div className="divider">OR</div>
